4ff6ed62a055f7ec2ff402b5fa66d91267ff0748
BictorVelovolov <112112051+BictorVelovolov@users.noreply.github.com>
2023-02-06 15:12:18 +0300

    Add files via upload

---
+++ vec2.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+#include"easy-list.h"
+using namespace std;
+
+void itc_rev_list(const vector <int> &mass)//Number 6
+{
+    int i;
+    if (mass.size() > 0)
+        for (i = mass.size(); i > 0; i--)
+            cout << mass[i - 1] << " ";
+}
+
+void itc_rev_par_list(const vector <int> &mass)//Number 7
+{
+    int i;
+        for (i = 0; i < mass.size(); i++)
+        {
+            if(i % 2 == 0)
+                cout << mass[i + 1] << " ";
+            else
+            if (i == (mass.size() - 1))
+                cout << mass[i];
+            else
+                cout << mass[i - 1] << " ";
+
+        }
+
+}
