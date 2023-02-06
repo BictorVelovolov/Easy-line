54c9613877b3a9d9289334a1831bf2a3fd8c616b
BictorVelovolov <112112051+BictorVelovolov@users.noreply.github.com>
2023-02-06 15:10:11 +0300

    Update list1.cpp

--- list1.cpp
+++ list1.cpp
@@ -3,27 +3,51 @@
 #include"easy-list.h"
 using namespace std;
 
-void itc_even_index_list(const vector <int> &mass1, vector <int> &mass2){
+void itc_even_index_list(const vector <int> &mass, vector <int> &mass2) //Number 1
+{
     int i;
-    for(i = 0; i < mass1.size(); i++)
-    {
-        if (i % 2 == 0)
-            mass2.push_back(mass1[i]);
-    }
+    if (mass.size() > 0)
+        for(i = 0; i < mass.size(); i++)
+            if (i % 2 == 0)
+                mass2.push_back(mass[i]);
 }
 
-void itc_even_part_list (const vector <int> &mass1, vector <int> &mass2){
+void itc_even_part_list (const vector <int> &mass1, vector <int> &mass2)//Number 2
+{
     int i;
     for (i = 0; i < mass1.size(); i++)
         if(mass1[i] % 2 == 0)
             mass2.push_back(mass1[i]);
 }
 
-int itc_positive_list(const vector <int> &mass)
+int itc_positive_list(const vector <int> &mass) //Number 3
 {
     int i, kol;
+    kol = 0;
+    if (mass.size() > 0)
     for (i = 0; i < mass.size(); i++)
         if (mass[i] > 0)
+            kol++;
+    return kol;
+}
+
+int itc_sl_list(const vector <int> &mass)//Number 4
+{
+    int i, kol;
+    kol = 0;
+    if (mass.size() > 0)
+    for (i = 0; i < mass.size(); i++)
+        if (mass[i] > mass[i - 1])
+            kol++;
+    return kol;
+}
+
+bool itc_same_parts_list(const vector <int> &mass)//Number 5
+{
+    int i, kol;
+    kol = 0;
+    for (i = 0; i < mass.size(); i++)
+        if((mass[i] > 0 && mass[i - 1] > 0) || (mass[i] < 0 && mass[i - 1] < 0))
             kol++;
     return kol;
 }
