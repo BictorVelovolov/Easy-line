b3db80eff5e90fa96683921237bddbd4987924ce
BictorVelovolov <112112051+BictorVelovolov@users.noreply.github.com>
2023-02-06 13:45:02 +0300

    Update list1.cpp

--- list1.cpp
+++ list1.cpp
@@ -18,3 +18,12 @@ void itc_even_part_list (const vector <int> &mass1, vector <int> &mass2){
         if(mass1[i] % 2 == 0)
             mass2.push_back(mass1[i]);
 }
+
+int itc_positive_list(const vector <int> &mass)
+{
+    int i, kol;
+    for (i = 0; i < mass.size(); i++)
+        if (mass[i] > 0)
+            kol++;
+    return kol;
+}
