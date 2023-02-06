11686cbca99dbbc3a6147df660a798dec4d784f6
BictorVelovolov <112112051+BictorVelovolov@users.noreply.github.com>
2023-02-06 13:45:18 +0300

    Update easy-list.h

--- easy-list.h
+++ easy-list.h
@@ -4,6 +4,7 @@
 using namespace std;
 void itc_even_index_list(const vector <int> &mass1, vector <int> &mass2);
 void itc_even_part_list (const vector <int> &mass1, vector <int> &mass2);
+int itc_positive_list(const vector <int> &mass);
 
 
 #endif // EASY-LIST_H_INCLUDED
