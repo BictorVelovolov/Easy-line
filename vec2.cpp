513ecbe154b48ff6b5f28b4a188e743b57946453
BictorVelovolov <112112051+BictorVelovolov@users.noreply.github.com>
2023-02-08 15:14:11 +0300

    Update vec2.cpp

--- vec2.cpp
+++ vec2.cpp
@@ -3,7 +3,7 @@
 #include"easy_list.h"
 using namespace std;
 
-void itc_rev_list(const vector <int> &mass)//Number 6
+void itc_rev_list(vector <int> &mass)//Number 6
 {
     int i;
     if (mass.size() > 0)
@@ -11,19 +11,26 @@ void itc_rev_list(const vector <int> &mass)//Number 6
             cout << mass[i - 1] << " ";
 }
 
-void itc_rev_par_list(const vector <int> &mass)//Number 7
+void itc_rev_par_list(vector <int> &mass)//Number 7
 {
     int i;
-        for (i = 0; i < mass.size(); i++)
+        for (i = 0; i < mass.size(); i = i + 2)
         {
-            if(i % 2 == 0)
-                cout << mass[i + 1] << " ";
+            if (mass.size() % 2 == 0)
+            {
+                mass[i] = mass[i+1] + mass[i];
+                mass[i+1] = mass[i] - mass[i+1];
+                mass[i] = mass[i] - mass[i+1];
+            }
             else
-            if (i == (mass.size() - 1))
-                cout << mass[i];
-            else
-                cout << mass[i - 1] << " ";
-
+            {
+                if (i != mass.size() - 1)
+                {
+                    mass[i] = mass[i+1] + mass[i];
+                    mass[i+1] = mass[i] - mass[i+1];
+                    mass[i] = mass[i] - mass[i+1];
+                }
+            }
         }
 
 }
