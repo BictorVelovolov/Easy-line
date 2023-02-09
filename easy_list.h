a259e2bce70ac1b7982fb364d4cfb5a401f7c7a2
BictorVelovolov <112112051+BictorVelovolov@users.noreply.github.com>
2023-02-09 23:56:43 +0300

    Update easy_list.h

--- easy_list.h
+++ easy_list.h
@@ -7,7 +7,8 @@ void itc_even_parts_list (const vector <int> &mass, vector <int> &mass2); //№2
 int itc_positive_list(const vector <int> &mass); //№3
 int itc_sl_list(const vector <int> &mass); //№4
 bool itc_same_parts_list(const vector <int> &mass); //№5
-void itc_rev_list(const vector <int> &mass); //№6
-void itc_rev_par_list(const vector <int> &mass); //№7
+void itc_rev_list(vector <int> &mass); //№6
+void itc_rev_par_list(vector <int> &mass); //№7
+void itc_rshift_list(vector <int> &mass);//№8
 
 #endif // EASY-LIST_H_INCLUDED
