ee2bc0d537bff53826b3321cf2884b0c5f56ea64
BictorVelovolov <112112051+BictorVelovolov@users.noreply.github.com>
2023-02-09 23:56:58 +0300

    Update easy_list.h

--- easy_list.h
+++ easy_list.h
@@ -11,4 +11,4 @@ void itc_rev_list(vector <int> &mass); //№6
 void itc_rev_par_list(vector <int> &mass); //№7
 void itc_rshift_list(vector <int> &mass);//№8
 
-#endif // EASY-LIST_H_INCLUDED
+#endif // EASY_LIST_H_INCLUDED
